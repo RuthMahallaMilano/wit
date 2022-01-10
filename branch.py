b79d2a054505c8288c2bde0d955b8cf402a349a2
Ruth Mahalla <rutimilano@gmail.com>
2022-01-10 15:17:41 +0200

    finish branch

--- branch.py
+++ branch.py
@@ -60,7 +60,6 @@ def commit(message: str) -> None:
     create_commit_txt_file(dir_with_wit, images_path, path_of_new_folder, message)
     save_files(dir_with_wit, path_of_new_folder)
     commit_id = os.path.basename(path_of_new_folder)
-
     write_references(commit_id, dir_with_wit)
 
 
@@ -116,20 +115,20 @@ def write_references(commit_id: str, dir_with_wit: Path) -> None:
     ref_file = wit_folder.joinpath('references.txt')
     activated_path = wit_folder.joinpath('activated.txt')
     activated_branch = activated_path.read_text()
-    new_content = '='.join((activated_branch, commit_id))
+    new_content = '='.join((activated_branch, commit_id))  # ???
     if ref_file.exists():
-        content = ref_file.read_text().split()
-        head_line = content[0]
-        head_id = head_line.split('=')[1]
-        branches_data = content[1:]
-        new_content = head_line
+        with open(ref_file, "r") as references_file:
+            data = references_file.readlines()
+        current_head_id = data[0].split("=")[1].strip()
+        branches_data = data[1:]
+        new_content = ""
         for line in branches_data:
-            name_and_id = line.split('=')
-            branch_name = name_and_id[0]
-            branch_id = name_and_id[1]
+            branch_name = line.split("=")[0]
+            branch_id = line.split("=")[1].strip()
             if branch_name == activated_branch:
-                if branch_id == head_id:
-                    line = '='.join((branch_name, commit_id))
+                activated_id = branch_id
+                if activated_id == current_head_id:
+                    line = "=".join((branch_name, commit_id))
             new_content += line
     ref_file.write_text(f'HEAD={commit_id}\n{new_content}')
 
@@ -214,7 +213,6 @@ def get_untracked_files(repository: Union[Path, str], staging_area_path: Union[P
 
 def checkout(commit_id_or_branch: str) -> None:
     repository = get_directory_with_wit(Path.cwd())
-    commit_path = os.path.join(repository, '.wit', 'images', commit_id_or_branch)
     staging_area_path = os.path.join(repository, '.wit', 'staging_area')
     last_commit_id = get_parent_head(repository)
     last_commit_path = os.path.join(repository, '.wit', 'images', last_commit_id)
@@ -227,10 +225,14 @@ def checkout(commit_id_or_branch: str) -> None:
         raise FilesDoesntMatchError("There are files added or changed after last commit")
     references_file = repository.joinpath('.wit', 'references.txt')
     commit_id = get_commit_id_of_branch(commit_id_or_branch, references_file)
+    if commit_id != commit_id_or_branch:
+        write_activated(commit_id_or_branch, repository)
+    commit_path = os.path.join(repository, '.wit', 'images', commit_id)
     change_files_in_main_folder(commit_path, repository)
     change_head_in_references_file(commit_id, references_file)
     change_staging_area_folder(staging_area_path, commit_path)
-    write_activated(commit_id_or_branch, repository)
+    if commit_id != commit_id_or_branch:
+        write_activated(commit_id_or_branch, repository)
 
 
 def write_activated(commit_id_or_branch: str, repository: Path) -> None:
@@ -252,18 +254,10 @@ def get_commit_id_of_branch(commit_id_or_branch: str, references_file: Path) -> 
         for branch_line in branches_data:
             name_and_id = branch_line.split('=')
             branch_name = name_and_id[0]
-            branch_id = name_and_id[1]
+            branch_id = name_and_id[1].strip()
             if branch_name == commit_id_or_branch:
                 return branch_id
     return commit_id_or_branch
-
-
-def get_master_commit_id(references_file: Path) -> str:
-    with references_file.open() as file:
-        references_content = file.readlines()
-        master_line = references_content[-1].split('=')
-        commit_id = master_line[-1].strip()
-    return commit_id
 
 
 def change_files_in_main_folder(commit_path: str, repository: Path) -> None:
@@ -275,6 +269,30 @@ def change_files_in_main_folder(commit_path: str, repository: Path) -> None:
         path_in_repository = os.path.join(repository, committed_file)
         with open(path_in_repository, "w") as original_file:
             original_file.write(content)
+
+
+# בהתחלה עשיתי את הפונקציה כמו שמופיע למטה בהמשך ההערה, והערת לי שיש דרך הרבה יותר פשוטה להעתיק תיקיה
+# ושיש פונקציה קיימת שיכולה לעזור לי, ולכן שינתי את הפונקציה כמו שמופיע למעלה.
+# אבל ככה יש בעיה במקרה והתווסף קובץ חדש, ורוצים לעשות checkout לקומיט מלפני יצירת הקובץ,
+# ואז בעצם בעידכון הקבצים בתיקיה צריך למחוק את הקובץ לחלוטין.
+# לא הצלחתי לחשוב על פתרון פשוט לזה חוץ צלהשתמש בדרך המסורבלת יותר שאיתה התחלתי.
+# אשמח לתובנות.
+#
+# def change_files_in_main_folder(repository: Path, commit_id: str) -> None:
+#     commit_path = os.path.join(repository, '.wit', 'images', commit_id)
+#     all_files_in_repository_and_sub_dirs = []
+#     for (root, dirs, files) in os.walk(repository, topdown=True):
+#         if '.wit' not in Path(root).parts:
+#             for file in files:
+#                 all_files_in_repository_and_sub_dirs.append(os.path.join(root, file))
+#     all_files_in_commit_and_sub_dirs = []
+#     for (root, dirs, files) in os.walk(commit_path, topdown=True):
+#         for file in files:
+#             all_files_in_commit_and_sub_dirs.append(os.path.join(root, file))
+#     for c_file in all_files_in_commit_and_sub_dirs:
+#         for r_file in all_files_in_repository_and_sub_dirs:
+#             if Path(r_file).relative_to(repository) == Path(c_file).relative_to(commit_path):
+#                 shutil.copy2(c_file, r_file)
 
 
 def change_staging_area_folder(staging_area_path: str, commit_path: str):
