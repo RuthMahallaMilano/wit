2e208911362a1a7fec01fce80cff90b8d8f19642
Ruth Mahalla <rutimilano@gmail.com>
2022-03-09 08:43:31 +0200

    change init function - no arguments

--- project/init.py
+++ project/init.py
@@ -1,17 +1,11 @@
 import os
 from pathlib import Path
-from typing import Optional
 
 from errors import WitExistsError
 
 
 def init_function() -> None:
     cwd = os.getcwd()
-    # if folder_name:
-    #     if os.path.isabs(folder_name):
-    #         cwd = folder_name
-    #     else:
-    #         cwd = os.path.join(cwd, folder_name)
     wit_path = os.path.join(cwd, '.wit')
     if os.path.exists(wit_path):
         raise WitExistsError("The folder already has .wit directory")
